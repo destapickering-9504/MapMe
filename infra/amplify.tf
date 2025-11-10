a3569cb4c7aeaa01bb139c12d5d9bcec5fd3800d
Desta Pickering <destapickering@Destas-MacBook-Pro.local>
2025-11-10 15:50:41 -0800

    removing future files to make implementation easier for now

--- infra/amplify.tf
+++
@@ -1,6 +0,0 @@
-# AWS Amplify Hosting - DISABLED
-# Commented out in favor of S3 + CloudFront deployment via GitHub Actions
-# Frontend is now deployed via GitHub Actions to S3 + CloudFront
-# See .github/workflows/frontend-deploy.yml for deployment pipeline
-
-# If you need to re-enable Amplify hosting, refer to git history or documentation
--- infra/variables.tf
+++ infra/variables.tf
@@ -4,25 +4,6 @@ variable "aws_region" {
   default     = "us-west-1"
 }
 
-variable "amplify_repo" {
-  description = "Git repository URL for Amplify hosting"
-  type        = string
-  default     = "https://github.com/dummy/repo"
-}
-
-variable "amplify_access_token" {
-  description = "Access token for Amplify to connect to your repo"
-  type        = string
-  sensitive   = true
-  default     = "dummy"
-}
-
-variable "amplify_branch" {
-  description = "Repository branch to build"
-  type        = string
-  default     = "main"
-}
-
 variable "app_callback_urls" {
   description = "Allowed redirect URIs after sign-in"
   type        = list(string)
@@ -33,54 +14,6 @@ variable "app_logout_urls" {
   description = "Allowed sign-out redirect URIs"
   type        = list(string)
   default     = ["http://localhost:3000"]
-}
-
-variable "google_client_id" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
-}
-
-variable "google_client_secret" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
-}
-
-variable "facebook_app_id" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
-}
-
-variable "facebook_app_secret" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
-}
-
-variable "apple_team_id" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
-}
-
-variable "apple_services_id" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
-}
-
-variable "apple_key_id" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
-}
-
-variable "apple_private_key_p8" {
-  type      = string
-  sensitive = true
-  default   = "dummy"
 }
 
 variable "cognito_domain_prefix" {
